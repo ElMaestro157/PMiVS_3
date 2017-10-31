0bac472672d3296a1fa827b8944023bace93527d
Dmitry Popov <Dimapopov.ru@yandex.ru>
2017-10-31 08:59:39 +0300

    Adding last projects

---
+++ ManyActivityProgram/app/src/main/java/com/example/dmitry/manyactivityprogram/Philippini.java
@@ -0,0 +1,14 @@
+package com.example.dmitry.manyactivityprogram;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Philippini extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.philippini);
+    }
+}
